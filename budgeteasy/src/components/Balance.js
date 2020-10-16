b566c1a5c237b541acc5fcc380fd17b5424c17e6
molly soufrine <mollysoufrine@gmail.com>
2020-10-16 13:18:18 -0400

    made the component for the balance, imported it to app.js

---
+++ budgeteasy/src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Balance = () => {
+  return (
+    //got rid of the div and just using a fragment
+    <>
+      <h4>Your Balance</h4>
+      <h1 id="balance">$0.00</h1>
+    </>
+  );
+};
