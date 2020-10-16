e426e9f10251e7179a394c106bbd83b1bf187fdc
molly soufrine <mollysoufrine@gmail.com>
2020-10-16 16:00:39 -0400

    changed props to transaction because i couldnt get the amount to show up with props. now the diff amount and items show along with a neg or pos sign

--- budgeteasy/src/components/Transaction.js
+++ budgeteasy/src/components/Transaction.js
@@ -1,11 +1,17 @@
 import React from "react";
 //since transaction is a prop here we must pass in a prop to the function, we can destructure and use ({transaction}) or just use props and props.text
-export const Transaction = (props) => {
+export const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
+  //ternary op to detect if amount is less than 0 its negative/exp, otherwise its pos and an inc
   return (
     <li className="minus">
-      {props.text}
-      <span>-$400</span>
+      {transaction.text}
+      <span>
+        {sign}${Math.abs(transaction.amount)}
+        {/* this is wrapped in math.abs so that two negative signs do not show up */}
+      </span>
       <button className="delete-btn">x</button>
     </li>
   );
 };
+//we need to write a function to recognize the sign + or -
