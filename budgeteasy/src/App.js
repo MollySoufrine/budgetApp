11570bcd5dc3a8eee0413e8d18953516ab868e0d
molly soufrine <mollysoufrine@gmail.com>
2020-10-16 13:19:03 -0400

    added some comments in app.js and balance.js

--- budgeteasy/src/App.js
+++ budgeteasy/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
+//using curly braces around the components to import them because they do not have an export default, we are using a const export
 import "./App.css";
 function App() {
   return (
