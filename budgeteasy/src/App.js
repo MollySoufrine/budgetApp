3347f64f8e95be3a9718010033b92cb2d3dc4c42
molly soufrine <mollysoufrine@gmail.com>
2020-10-16 13:50:20 -0400

    added the transactionlist componenet and linked it to app.js. started to create the form component for someone to add a new exp. or inc

--- budgeteasy/src/App.js
+++ budgeteasy/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Header } from "./components/Header";
 import { Balance } from "./components/Balance";
 import { IncomeExpenses } from "./components/IncomeExpenses";
+import { TransactionList } from "./components/TransactionList";
 //using curly braces around the components to import them because they do not have an export default, we are using a const export
 import "./App.css";
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <div className="container">
         <Balance />
         <IncomeExpenses />
+        <TransactionList />
       </div>
     </div>
   );
