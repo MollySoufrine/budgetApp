725d226a30787a9391b935ad2d88d6445badfd0a
molly soufrine <mollysoufrine@gmail.com>
2020-10-16 15:31:24 -0400

    created our context and global state as well as a proovider so we can access transactions obj from any component, now creating our AppReducer

--- budgeteasy/src/context/GlobalState.js
+++ budgeteasy/src/context/GlobalState.js
@@ -1,5 +1,6 @@
 //where we will create context
 import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
 
 //set initial state
 const initialState = {
@@ -12,7 +13,26 @@ const initialState = {
   //exp is negative num, inc is pos number
 };
 
-//any global state will go in that obj above.
+/*
+any global state will go in that obj above.
+as long as we have access to the transactions in certain components we can do our calculations
+we just need to pass the data down then we will have actions to delete/add from/to the state
+*/
 
-//as long as we have access to the transactions in certain components we can do our calculations
-//we just need to pass the data down then we will have actions to delete/add from/to the state
+//create context
+export const GlobalContext = createContext(initialState);
+// in order for other components to have access to our state we need a provider
+
+//Provoder component
+//we want our components wrapped in a provider, this makes them children, so we destrcuture and pass in children
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+  //using reducer we want state and dispatch
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+//we pass the provider an obj. using state. allows you to access anything in the obj. and we want to access the transactions here
+//this allows us to access transaction from any component with useContext
