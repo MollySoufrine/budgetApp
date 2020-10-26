1ae714e2c47a3bce276825415086849dc332aa63
molly soufrine <mollysoufrine@gmail.com>
2020-10-26 11:43:00 -0400

    tried using functions to calc income and expense then pass them into hooks/useState and useEffect... it shows up on the screen correctly but will test further to see if it works this way. just wanted to try something different

--- budgeteasy/src/components/IncomeExpenses.js
+++ budgeteasy/src/components/IncomeExpenses.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  const [useIncome, setIncome] = useState(income);
+  const [useExpense, setExpense] = useState(expense);
+
+  useEffect(() => {
+    setExpense(expense);
+    setIncome(income);
+  });
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
         <p id="money-plus" className="money plus">
-          +$0.00
+          ${useIncome}
         </p>
       </div>
       <div>
         <h4>Expense</h4>
         <p id="money-minus" className="money minus">
-          -$0.00
+          ${useExpense}
         </p>
       </div>
     </div>
