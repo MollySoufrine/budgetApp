966bb61bbd99569952ad0b59c8565ce4c67ae6a0
molly soufrine <mollysoufrine@gmail.com>
2020-11-05 19:53:31 -0500

    Updates

---
+++ precache-manifest.6b6497909ce3c86645fd78aa6424db59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9bb8fe74be61d0145adfda0ed2c6c",
+    "url": "/budgetapp.io/index.html"
+  },
+  {
+    "revision": "a91704ad176d4b87665f",
+    "url": "/budgetapp.io/static/css/main.2a4cb120.chunk.css"
+  },
+  {
+    "revision": "1bb11862411334eff4af",
+    "url": "/budgetapp.io/static/js/2.03b6159c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budgetapp.io/static/js/2.03b6159c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91704ad176d4b87665f",
+    "url": "/budgetapp.io/static/js/main.347de631.chunk.js"
+  },
+  {
+    "revision": "70d309a56bab8e03d713",
+    "url": "/budgetapp.io/static/js/runtime-main.2c72350a.js"
+  }
+]);
